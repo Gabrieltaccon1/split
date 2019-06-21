2f151504a8c1397c584f3fa96f14f36de732e607
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-21 15:44:51 -0300

    split

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frases;
+            
+            Console.WriteLine("DIGITE AS 4  PALAVRAS DESEJADAS EM UMA UNICA LINHA SEPARANDO POR ESPAÇOS");
+            frases = Console.ReadLine();
+
+            string[] vet = frases.Split(' ');
+
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+            string p4 = vet[3];
+
+            Console.WriteLine("\nVOCÊ DIGITOU: \n");
+            Console.WriteLine(frases);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+        }
+    }
+}
